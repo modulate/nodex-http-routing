92a6a23fd72edc66ab9eadbf042b91baa558253f
Jared Hanson <jaredhanson@gmail.com>
2016-07-09 14:25:55 -0300

    Implement basic forwarding.

--- xom/routing/forward.js
+++ xom/routing/forward.js
@@ -1,6 +1,9 @@
-exports = module.exports = function() {
-  return require('../../lib/forwarding')();
+exports = module.exports = function(streams) {
+  return require('../../lib/forwarding')(streams);
 }
 
 exports['@singleton'] = true;
 exports['@implements'] = 'http://schemas.modulate.io/js/http/routing/forwardingFunc';
+exports['@require'] = [
+  'http://i.bixbyjs.org/stream/Factory'
+];
