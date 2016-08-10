6ec93c853e7db363465d405b361467b1f01adb90
Jared Hanson <jaredhanson@gmail.com>
2016-08-09 19:14:34 -0700

    Add comments.

--- xom/routing/table.js
+++ xom/routing/table.js
@@ -1,12 +1,13 @@
-exports = module.exports = function(instances, services, hosts) {
+exports = module.exports = function(resolver, instances, services, hosts) {
   var Table = require('../../lib/table');
   
-  return new Table(instances, services, hosts);
+  return new Table(resolver, instances, services, hosts);
 }
 
 exports['@singleton'] = true;
 exports['@implements'] = 'http://schemas.modulate.io/js/http/routing/Table';
 exports['@require'] = [
+  'http://i.bixbyjs.org/ns/Resolver',
   'http://schemas.modulate.io/js/http/routing/topology/backend/InstancesTable',
   'http://schemas.modulate.io/js/http/routing/topology/backend/ServicesTable',
   'http://schemas.modulate.io/js/http/routing/topology/backend/URLsMap'
